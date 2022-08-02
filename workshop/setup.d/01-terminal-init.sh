2a62a8d2caaf87b666ed2eb84fa0779d24ad4b00
dineshtripathi30 <dinesh.tripathi30@gmail.com>
2022-08-02 12:18:25 +0530

    updated file

--- workshop/setup.d/01-terminal-init.sh
+++ workshop/setup.d/01-terminal-init.sh
@@ -1,9 +1,9 @@
 export REGISTRY_HOST=tanzudemoreg.azurecr.io
-
+echo $REGISTRY_HOST
 export USERNAME1=`kubectl get secret registry-credentials -n default -o json | jq '.data' | cut -f2 -d":"|tr -d \" | awk 'NR>1 && NR < 3'`
 
 export USERNAME=$(echo $USERNAME1 | base64 -d | jq '.auths[].username' | tr -d '"')
-
+echo $USERNAME
 export PASSWORD1=`kubectl get secret registry-credentials -n default -o json | jq '.data' | cut -f2 -d":"|tr -d \" | awk 'NR>1 && NR < 3'`
 
 export PASSWORD=$(echo $PASSWORD1 | base64 -d | jq '.auths[].password' | tr -d '"')
