1d7a899b99f9dafd27f9eaa550c09413b94cbeee
dineshtripathi30 <dinesh.tripathi30@gmail.com>
2022-08-02 13:28:07 +0530

    updated file

--- Dockerfile
+++ Dockerfile
@@ -26,10 +26,12 @@ RUN apt-get update && apt-get install -y unzip openjdk-11-jdk
 
 # Requirements for Live Update
 RUN curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 4.3.0
-RUN mv /usr/bin/code-server /opt/code-server/bin/code-server
-COPY extensions/tanzu-vscode-extension.vsix /tmp
+#RUN mv /usr/bin/code-server /opt/code-server/bin/code-server
+COPY extensions/tanzu-vscode-extension-0.7.1+build.1.vsix /tmp
+COPY extensions/tanzu-app-accelerator-0.1.2.vsix /tmp
 RUN code-server --install-extension vscjava.vscode-java-pack && \
-    code-server --install-extension /tmp/tanzu-vscode-extension.vsix
+    code-server --install-extension /tmp/tanzu-vscode-extension-0.7.1+build.1.vsix \
+    code-server --install-extension /tmp/tanzu-app-accelerator-0.1.2.vsix
 RUN echo -n 'export PATH=~/.local/bin:$PATH' >> /etc/profile
 RUN chown -R eduk8s:users /home/eduk8s/.cache
 RUN chown -R eduk8s:users /home/eduk8s/.local
--- workshop/setup.d/01-terminal-init.sh
+++ workshop/setup.d/01-terminal-init.sh
@@ -2,13 +2,13 @@ export REGISTRY_HOST=tanzudemoreg.azurecr.io
 echo $REGISTRY_HOST
 export USERNAME1=`kubectl get secret registry-credentials -n default -o json | jq '.data' | cut -f2 -d":"|tr -d \" | awk 'NR>1 && NR < 3'`
 
-export USERNAME=$(echo $USERNAME1 | base64 -d | jq '.auths[].username' | tr -d '"')
-echo $USERNAME
+export REGISTRY_USERNAME=$(echo $USERNAME1 | base64 -d | jq '.auths[].username' | tr -d '"')
+echo $REGISTRY_USERNAME
 export PASSWORD1=`kubectl get secret registry-credentials -n default -o json | jq '.data' | cut -f2 -d":"|tr -d \" | awk 'NR>1 && NR < 3'`
 
-export PASSWORD=$(echo $PASSWORD1 | base64 -d | jq '.auths[].password' | tr -d '"')
+export REGISTRY_PASSWORD=$(echo $PASSWORD1 | base64 -d | jq '.auths[].password' | tr -d '"')
 # Login to Docker Registry
-docker login $REGISTRY_HOST -u $USERNAME -p $PASSWORD
+docker login $REGISTRY_HOST -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD
 
 # Rename eduk8s context to tap cluster name
 
@@ -17,3 +17,9 @@ kubectl config rename-context eduk8s tap11-aks-fullcluster
 # Switch to default namespace
 kubectl config set-context --current --namespace default
 
+# Get GIT Creds to push changes
+export GIT_USERNAME1=`kubectl get secret git-secret -n default -o json | jq '.data.username' | tr -d '"'`
+export GIT_PASSWORD1=`kubectl get secret git-secret -n default -o json | jq '.data.password' | tr -d '"'`
+export GIT_USERNAME=$(echo $GIT_USERNAME1 | base64 -d | tr -d '"')
+#echo $GIT_USERNAME
+export GIT_PASSWORD=$(echo $GIT_PASSWORD1 | base64 -d | tr -d '"')
