4df0688ee1fdbdf5a6fab34d13721259bd787782
Allaeddine Elareed <aelareed@vmware.com>
2022-08-04 16:46:30 +0200

    fix shell scripts

--- workshop/setup.d/00-gitops-repo.sh
+++ workshop/setup.d/00-gitops-repo.sh
@@ -21,7 +21,7 @@ git add .
 git commit -a -m "Initial Commit"
 
 
-git remote add origin https://$GIT_USERNAME:$GITEA_PASSWORD@${GIT_HOST}/tapgit/$REPO_NAME.git
+git remote add origin https://$GIT_USERNAME:$GIT_PASSWORD@${GIT_HOST}/tapgit/$REPO_NAME.git
 git push -u origin main
 
 cd ..
