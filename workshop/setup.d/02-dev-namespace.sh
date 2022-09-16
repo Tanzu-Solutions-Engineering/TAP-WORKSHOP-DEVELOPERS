6b12177065083247d40c7b4ad635d67eef29a416
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-09-16 12:16:12 +0530

    Update 02-dev-namespace.sh

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -11,6 +11,6 @@ export REGISTRY_USERNAME=$(kubectl get secret registry-credentials -n tap-instal
 export REG_PASSWORD=$(kubectl get secret registry-credentials -n tap-install -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzupartnerworkshop.azurecr.io".password')
 
 
-REGISTRY_PASSWORD=$REG_PASSWORD kp secret create registry-credentials --registry ${REGISTRY_HOST} --registry-user $REGISTRY_USERNAME
+# REGISTRY_PASSWORD=$REG_PASSWORD kp secret create registry-credentials --registry ${REGISTRY_HOST} --registry-user $REGISTRY_USERNAME
 
-REGISTRY_PASSWORD=$REG_PASSWORD kp secret create tanzu-tap-repository-fetch-0 --registry ${REGISTRY_HOST} --registry-user $REGISTRY_USERNAME
+# REGISTRY_PASSWORD=$REG_PASSWORD kp secret create tanzu-tap-repository-fetch-0 --registry ${REGISTRY_HOST} --registry-user $REGISTRY_USERNAME
