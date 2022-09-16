f66e109579b04283100dd2c5d2bd82aa67773455
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-09-16 10:38:17 +0530

    Update 02-dev-namespace.sh

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -2,13 +2,13 @@
 set -x
 set +e
 
-export REGISTRY_HOST=tanzudemoreg.azurecr.io
+export REGISTRY_HOST=tanzupartnerworkshop.azurecr.io
 
-export REGISTRY_USERNAME=$(kubectl get secret registry-credentials -n default -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzudemoreg.azurecr.io".username')
+export REGISTRY_USERNAME=$(kubectl get secret registry-credentials -n tap-install -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzudemoreg.azurecr.io".username')
 
 #echo $REGISTRY_USERNAME
 
-export REG_PASSWORD=$(kubectl get secret registry-credentials -n default -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzudemoreg.azurecr.io".password')
+export REG_PASSWORD=$(kubectl get secret registry-credentials -n tap-install -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzudemoreg.azurecr.io".password')
 
 
 REGISTRY_PASSWORD=$REG_PASSWORD kp secret create registry-credentials --registry ${REGISTRY_HOST} --registry-user $REGISTRY_USERNAME
