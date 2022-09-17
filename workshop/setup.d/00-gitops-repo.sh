a623ca396237f32077aec34e53c70565b66c5ae2
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-09-17 16:00:23 +0530

    Update 00-gitops-repo.sh

--- workshop/setup.d/00-gitops-repo.sh
+++ workshop/setup.d/00-gitops-repo.sh
@@ -11,6 +11,21 @@ export GIT_USERNAME=$(kubectl get secret gitea-secret -n tap-install -o json | j
 export GIT_PASSWORD=$(kubectl get secret gitea-secret -n tap-install -o json | jq -r '.data.password' | base64 -d)
 export GIT_HOST=gitea-tapdemo.captainvirtualization.in
 
+export USERNAME1=`kubectl get secret registry-credentials -n tap-install -o json | jq '.data' | cut -f2 -d":"|tr -d \" | awk 'NR>1 && NR < 3'`
+
+export REGISTRY_USERNAME=$(echo $USERNAME1 | base64 -d | jq '.auths[].username' | tr -d '"')
+
+echo $REGISTRY_USERNAME
+
+export PASSWORD1=`kubectl get secret registry-credentials -n tap-install -o json | jq '.data' | cut -f2 -d":"|tr -d \" | awk 'NR>1 && NR < 3'`
+
+export REGISTRY_PASSWORD=$(echo $PASSWORD1 | base64 -d | jq '.auths[].password' | tr -d '"')
+
+export REGISTRY_HOST=tanzupartnerworkshop.azurecr.io
+
+# Login to Docker Registry
+docker login $REGISTRY_HOST -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD
+
 mkdir partnertapdemo
 cd partnertapdemo
 echo "# TAP Demo for Tanzu Partners" >> README.md
