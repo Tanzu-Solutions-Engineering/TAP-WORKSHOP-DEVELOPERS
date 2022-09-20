1563a6d011865fe9599b2e8b900497081341f01d
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-09-20 18:28:38 +0530

    Update 00-gitops-repo.sh

--- workshop/setup.d/00-gitops-repo.sh
+++ workshop/setup.d/00-gitops-repo.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
-set -x
-set +e
+#set -x
+#set +e
 
-export REPO_NAME=$SESSION_NAMESPACE-$(date +%s)
-echo $REPO_NAME > repo.txt
-REPO_NAME=$(cat repo.txt)
+#export REPO_NAME=$SESSION_NAMESPACE-$(date +%s)
+#echo $REPO_NAME > repo.txt
+#REPO_NAME=$(cat repo.txt)
 
-export GIT_USERNAME=$(kubectl get secret gitea-secret -n tap-install -o json | jq -r '.data.username' | base64 -d)
-export GIT_PASSWORD=$(kubectl get secret gitea-secret -n tap-install -o json | jq -r '.data.password' | base64 -d)
-export GIT_HOST=gitea-tapdemo.captainvirtualization.in
+#export GIT_USERNAME=$(kubectl get secret gitea-secret -n tap-install -o json | jq -r '.data.username' | base64 -d)
+#export GIT_PASSWORD=$(kubectl get secret gitea-secret -n tap-install -o json | jq -r '.data.password' | base64 -d)
+#export GIT_HOST=gitea-tapdemo.captainvirtualization.in
 
-export USERNAME1=`kubectl get secret registry-credentials -n tap-install -o json | jq '.data' | cut -f2 -d":"|tr -d \" | awk 'NR>1 && NR < 3'`
+#export USERNAME1=`kubectl get secret registry-credentials -n tap-install -o json | jq '.data' | cut -f2 -d":"|tr -d \" | awk 'NR>1 && NR < 3'`
 
-export REGISTRY_USERNAME=$(echo $USERNAME1 | base64 -d | jq '.auths[].username' | tr -d '"')
+#export REGISTRY_USERNAME=$(echo $USERNAME1 | base64 -d | jq '.auths[].username' | tr -d '"')
 
-echo $REGISTRY_USERNAME
+#echo $REGISTRY_USERNAME
 
-export PASSWORD1=`kubectl get secret registry-credentials -n tap-install -o json | jq '.data' | cut -f2 -d":"|tr -d \" | awk 'NR>1 && NR < 3'`
+#export PASSWORD1=`kubectl get secret registry-credentials -n tap-install -o json | jq '.data' | cut -f2 -d":"|tr -d \" | awk 'NR>1 && NR < 3'`
 
-export REGISTRY_PASSWORD=$(echo $PASSWORD1 | base64 -d | jq '.auths[].password' | tr -d '"')
+#export REGISTRY_PASSWORD=$(echo $PASSWORD1 | base64 -d | jq '.auths[].password' | tr -d '"')
 
-export REGISTRY_HOST=tanzupartnerworkshop.azurecr.io
+#export REGISTRY_HOST=tanzupartnerworkshop.azurecr.io
 
 # Login to Docker Registry
-docker login $REGISTRY_HOST -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD
+#docker login $REGISTRY_HOST -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD
 
-mkdir partnertapdemo
-cd partnertapdemo
-echo "# TAP Demo for Tanzu Partners" >> README.md
-git init
-git checkout -b main
-git config user.name $GIT_USERNAME
-git config user.email "$GIT_USERNAME@gitea.com"
-git add .
-git commit -a -m "Initial Commit"
+#mkdir partnertapdemo
+#cd partnertapdemo
+#echo "# TAP Demo for Tanzu Partners" >> README.md
+#git init
+#git checkout -b main
+#git config user.name $GIT_USERNAME
+#git config user.email "$GIT_USERNAME@gitea.com"
+#git add .
+#git commit -a -m "Initial Commit"
 
 
-git remote add origin https://$GIT_USERNAME:$GIT_PASSWORD@${GIT_HOST}/$GIT_USERNAME/$REPO_NAME.git
-git push -u origin main
+#git remote add origin https://$GIT_USERNAME:$GIT_PASSWORD@${GIT_HOST}/$GIT_USERNAME/$REPO_NAME.git
+#git push -u origin main
 
-cd ..
+#cd ..
 # git clone https://gitea_admin:$GITEA_PASSWORD@gitea.${INGRESS_DOMAIN}/gitea_admin/gitops-workloads.git
 # git -C /home/eduk8s/gitops-workloads config user.name gitea_admin
 # git -C /home/eduk8s/gitops-workloads config user.email "gitea_admin@example.com"
