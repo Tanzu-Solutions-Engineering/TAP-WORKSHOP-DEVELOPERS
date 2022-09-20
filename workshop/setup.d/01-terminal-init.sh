e0959808ed0ed2b205e144aa56b87993d38b5ac1
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-09-20 18:27:18 +0530

    Update 01-terminal-init.sh

--- workshop/setup.d/01-terminal-init.sh
+++ workshop/setup.d/01-terminal-init.sh
@@ -15,3 +15,24 @@ echo $REGISTRY_HOST
 # Login to Docker Registry
 
 docker login $REGISTRY_HOST -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD
+
+export REPO_NAME=$SESSION_NAMESPACE-$(date +%s)
+
+export GIT_USERNAME=$(kubectl get secret gitea-secret -n tap-install -o json | jq -r '.data.username' | base64 -d)
+export GIT_PASSWORD=$(kubectl get secret gitea-secret -n tap-install -o json | jq -r '.data.password' | base64 -d)
+export GIT_HOST=gitea-tapdemo.captainvirtualization.in
+
+mkdir partnertapdemo
+cd partnertapdemo
+echo "# TAP Demo for Tanzu Partners" >> README.md
+git init
+git checkout -b main
+git config user.name $GIT_USERNAME
+git config user.email "$GIT_USERNAME@gitea.com"
+git add .
+git commit -a -m "Initial Commit"
+
+cd ..
+
+git remote add origin https://$GIT_USERNAME:$GIT_PASSWORD@${GIT_HOST}/$GIT_USERNAME/$REPO_NAME.git
+git push -u origin main
