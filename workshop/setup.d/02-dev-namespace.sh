74a716006fdc4f088289f7078c14b19ed53d2543
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-10-14 14:08:43 +0530

    Update 02-dev-namespace.sh

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -2,24 +2,15 @@
 set -x
 set +e
 
-echo "I am done"
+export REGISTRY_HOST=tanzupartnerworkshop.azurecr.io
 
-printf %b "$KUBE_CONFIG" > config
-envsubst < config > .kube/config
+export REGISTRY_USERNAME=$(kubectl get secret registry-credentials -n tap-install -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzupartnerworkshop.azurecr.io".username')
 
-envsubst < /home/eduk8s/install/rbac/app-editor.yaml | kubectl apply -f-
+echo $REGISTRY_USERNAME
 
-echo "I am done again"
-
-#export REGISTRY_HOST=tanzupartnerworkshop.azurecr.io
-
-#export REGISTRY_USERNAME=$(kubectl get secret registry-credentials -n tap-install -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzupartnerworkshop.azurecr.io".username')
-
-#echo $REGISTRY_USERNAME
-
-#export REG_PASSWORD=$(kubectl get secret registry-credentials -n tap-install -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzupartnerworkshop.azurecr.io".password')
+export REG_PASSWORD=$(kubectl get secret registry-credentials -n tap-install -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzupartnerworkshop.azurecr.io".password')
 
 
-# REGISTRY_PASSWORD=$REG_PASSWORD kp secret create registry-credentials --registry ${REGISTRY_HOST} --registry-user $REGISTRY_USERNAME
+#REGISTRY_PASSWORD=$REG_PASSWORD kp secret create registry-credentials --registry ${REGISTRY_HOST} --registry-user $REGISTRY_USERNAME
 
-# REGISTRY_PASSWORD=$REG_PASSWORD kp secret create tanzu-tap-repository-fetch-0 --registry ${REGISTRY_HOST} --registry-user $REGISTRY_USERNAME
+#REGISTRY_PASSWORD=$REG_PASSWORD kp secret create tanzu-tap-repository-fetch-0 --registry ${REGISTRY_HOST} --registry-user $REGISTRY_USERNAME
