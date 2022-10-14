bc10788cdb91d4d49ee8093b11193e70ab60f05e
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-10-14 13:13:26 +0530

    Update env.sh

--- workshop/profile.d/env.sh
+++ workshop/profile.d/env.sh
@@ -1,43 +1,43 @@
 #echo "Hello from your Terminal"
 
-export REGISTRY_HOST=tanzupartnerworkshop.azurecr.io
+#export REGISTRY_HOST=tanzupartnerworkshop.azurecr.io
 
-export REGISTRY_USERNAME=$(kubectl get secret registry-credentials -n tap-install -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzudemoreg.azurecr.io".username')
+#export REGISTRY_USERNAME=$(kubectl get secret registry-credentials -n tap-install -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzudemoreg.azurecr.io".username')
 
 #echo $REGISTRY_USERNAME
 
-export REGISTRY_PASSWORD=$(kubectl get secret registry-credentials -n tap-install -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzudemoreg.azurecr.io".password')
+#export REGISTRY_PASSWORD=$(kubectl get secret registry-credentials -n tap-install -o json | jq -r '.data.".dockerconfigjson"' | base64 -d | jq -r '.auths."tanzudemoreg.azurecr.io".password')
 
 # Login to Docker Registry
 
-docker login $REGISTRY_HOST -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD
+#docker login $REGISTRY_HOST -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD
 
 # Rename eduk8s context to tap cluster name
 
-kubectl config rename-context eduk8s tap12-aks-fullcluster
+#kubectl config rename-context eduk8s tap12-aks-fullcluster
 
 # Switch to default namespace
 # kubectl config set-context --current --namespace default
 
 # Get GITea Creds to push changes
 
-export GIT_USERNAME=$(kubectl get secret gitea-secret -n tap-install -o json | jq -r '.data.username' | base64 -d)
-export GIT_PASSWORD=$(kubectl get secret gitea-secret -n tap-install -o json | jq -r '.data.password' | base64 -d)
-export GIT_HOST=gitea-tapdemo.tap.tanzupartnerdemo.com
+#export GIT_USERNAME=$(kubectl get secret gitea-secret -n tap-install -o json | jq -r '.data.username' | base64 -d)
+#export GIT_PASSWORD=$(kubectl get secret gitea-secret -n tap-install -o json | jq -r '.data.password' | base64 -d)
+#export GIT_HOST=gitea-tapdemo.tap.tanzupartnerdemo.com
 
 # Setup GIT for change commit
-git config --global user.email "$GIT_USERNAME@gitea.com"
-git config --global user.name $GIT_USERNAME
+#git config --global user.email "$GIT_USERNAME@gitea.com"
+#git config --global user.name $GIT_USERNAME
 
 
 # Other Variables
 
 #export ACC_SERVER_URL="https://accelerator.tap11.tanzupartnerdemo.com"
 
-export ARGOCD_USERNAME=admin
-export ARGOCD_PASSWORD=pwd
+#export ARGOCD_USERNAME=admin
+#export ARGOCD_PASSWORD=pwd
 
-clear
+#clear
 
 
 #exec bash
